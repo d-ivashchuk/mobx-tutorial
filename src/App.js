ffa7c6b937ab39cdf2f70ea280fa8e7fe2302be1
DmytroI <dimaiv@me.com>
2018-11-26 13:54:08 +0100

    preparing for ejection

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { observable } from 'mobx'
+import { observer } from 'mobx-react'
 
 class App extends Component {
+  count = 0
+  handleIncrement = () => {
+    this.count++
+  }
+  handleDecrement = () => {
+    this.count--
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1>{this.count}</h1>
+          <div>
+            <button onClick={this.handleDecrement}>{'<'}</button>
+            <button onClick={this.handleIncrement}>{'>'}</button>
+          </div>
+
         </header>
       </div>
     );
